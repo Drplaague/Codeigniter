4d26b08df5c0583960e79501f9bcbdb38a685161
Drplaague <juanaditia94@gmail.com>
2020-03-28 22:15:02 +0700

    frist commit

---
+++ application/modules/dash/views/v_dashboard.php
@@ -0,0 +1,17 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');?>
+
+
+<html>
+<header><title>This is title</title></header>
+<body>
+Hello world
+
+<div>
+          
+          <a href="<?php echo base_url('login/logout'); ?>">Logout</a>
+        </div>
+
+</body>
+</html>
+
+ 
